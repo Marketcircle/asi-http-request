cedf01be0f5fc292514924865700565c54d5785d
Thomas Bartelmess <tbartelmess@marketcircle.com>
2011-08-24 21:30:59 -0400

    Added Mac Framework

---
+++ ASIHTTP/ASIHTTP/ASIHTTP.h
@@ -0,0 +1,13 @@
+//
+//  ASIHTTP.h
+//  ASIHTTP
+//
+//  Created by Thomas Bartelmess on 11-08-16.
+//  Copyright (c) 2011 Marketcircle Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASIHTTP : NSObject
+
+@end
---
+++ ASIHTTP/ASIHTTP/ASIHTTP.m
@@ -0,0 +1,23 @@
+//
+//  ASIHTTP.m
+//  ASIHTTP
+//
+//  Created by Thomas Bartelmess on 11-08-16.
+//  Copyright (c) 2011 Marketcircle Inc. All rights reserved.
+//
+
+#import "ASIHTTP.h"
+
+@implementation ASIHTTP
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
